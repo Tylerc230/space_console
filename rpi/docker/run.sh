84d4707b29e6bb33aab54ddbf0f5695157c89d54
Tyler Casselman <tylerc230@gmail.com>
2021-10-27 16:52:31 -0700

    rpi + docker + ros

---
+++ rpi/docker/run.sh
@@ -0,0 +1,1 @@
+docker run --rm --mount src=/Users/tyler/code/Arduino/space_console/rpi/docker/../workspace,dst=/workspace,type=bind -it ros_arm:latest
---
+++ rpi/workspace/devel/share/cpp_test/cmake/cpp_testConfig-version.cmake
@@ -0,0 +1,1 @@
+/workspace/devel/.private/cpp_test/share/cpp_test/cmake/cpp_testConfig-version.cmake
