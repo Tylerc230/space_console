8c0f1f2fcdf3eb9cb316146b9f5d499ea7ef90d8
Tyler Casselman <cstyle@amazon.com>
2021-11-30 16:16:16 -0800

    Use interrupts for inputs

--- firmware-rust/avr/src/main.rs
+++ firmware-rust/avr/src/main.rs
@@ -1,4 +1,5 @@
 #![no_std]
+#![feature(abi_avr_interrupt)]
 #![no_main]
 
 use panic_halt as _;
@@ -17,10 +18,27 @@ use rotary_encoder_hal::Rotary;
 use firmware_rust::ufmt;
 use input_driver::InputDriver;
 
+
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins  = arduino_hal::pins!(dp);
+
+    dp.EXINT.pcicr.write(|w| {
+        w.pcie().bits(0b00000011u8)//Enable port B and C
+    });
+
+    dp.EXINT.pcmsk0.write(|w|{//port B
+        w.pcint().bits(0b00011100u8)//PB2, PB3, PB4 (ie d10, d11, d12)
+    });
+
+    dp.EXINT.pcmsk1.write(|w| {
+        w.pcint().bits(0b00001000u8)//PC3 (id a3)
+    });
+
+    unsafe {
+        avr_device::interrupt::enable();
+    }
 
     let serial = arduino_hal::default_serial!(dp, pins, 57600);
     serial::init(serial);
@@ -50,13 +68,22 @@ fn main() -> ! {
     //let switch = pins.a2.into_pull_up_input();
     let mut input = InputDriver::new(left_rotary, right_rotary);
     loop {
-        let input_values = input.update();
-        if input_values.left_rotary_direction != fr::input::KnobDirection::None || input_values.right_rotary_direction != fr::input::KnobDirection::None {
-            serial_println!("Input {:#?}\r", input_values).void_unwrap();
-        }
+        //let input_values = input.update();
+        //if input_values.left_rotary_direction != fr::input::KnobDirection::None || input_values.right_rotary_direction != fr::input::KnobDirection::None {
+            //serial_println!("Input {:#?}\r", input_values).void_unwrap();
+        //}
         runner.update();
         screen.write_buffer(&runner.pixel_buffer);
-        //arduino_hal::delay_ms(1000 as u16);
+        arduino_hal::delay_ms(1000 as u16);
     }
+}
+#[avr_device::interrupt(atmega328p)]
+fn PCINT0() { //or 1 or 2
+    serial_println!("portb hit\r").void_unwrap();
+}
+
+#[avr_device::interrupt(atmega328p)]
+fn PCINT1() {
+    serial_println!("portb hit\r").void_unwrap();
 }
 
